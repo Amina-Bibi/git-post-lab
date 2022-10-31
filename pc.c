a4b897c0eef0701ca3d58f5f1865563f5bff828f
MaryamKiran <116889152+MaryamKiran@users.noreply.github.com>
2022-10-31 21:27:46 +0500

    Update pc.c
    Applying validation Rules

--- pc.c
+++ pc.c
@@ -1,40 +1,50 @@
-#include<stdio.h>
+ #include<stdio.h>
 int main()
 {
     int a[100]={0};
     int n=0;
     int s;
     int c=1;
-    while(n!=3){//take till it enter 3
+    while(n!=3){
     printf("Type 1 for \"first class\"\nType 2 for \"economy class\"\n");
     printf("Enter 3 to exit\n");
+    
     printf("Select type of class:");
-    scanf("%d",&n);
-    while((n!=1) && (n!=2) && (n!=3)){//checking class id
+    scanf("%d",&n);//taking input for choosing class
+    while((n!=1) && (n!=2) && (n!=3))
+	{
     printf("Enter valid  id ");
     scanf("%d",&n);
     }
     if(n==1){
-    printf("Select seat number (1-30): ");
+    printf("Select seat number (1-30): ");//taking inpu for class 1
+    
     scanf("%d", &s);
-   
+    while(((s>30) || (s<1)) ||(a[s]==1))//seat validation for class 1
+	{
     	if(a[s]==1)
-     	   printf("Sorry Not available Enter another seat number\n");
+     	   printf("Sorry Not available Enter another seat number: ");
      	   else
-   
-	printf("\tBoarding Pass\nClass:\"First\"\nSeat number: %d\n",s); 
-	 a[s]=1;
-}
+    printf("Enter between 1-30: ");
+    	scanf("%d",&s);	
+	}
+    a[s]=1; 
+	printf("Class:\"First\"\nSeat number: %d\n",s); 
+    }
     else if(n==2){
-    printf("Select seat number (31-100):");
+    printf("Select seat number (31-100):");//taking input for class 2 
     scanf("%d", &s);
-  
+     while(((s>100) || (s<31)) ||(a[s]==1))//seat validation for class 2
+	 {
      	if(a[s]==1)
-     	   printf("Sorry Not available \n");
-     
-  else
-    	printf("\tBoarding Pass\nClass:\"First\"\nSeat number: %d\n",s); 
-    	 a[s]=1;
+     	   printf("Sorry Not available Enter another seat number: ");
+     	   else
+     printf("Enter between 31-100: ");
+    	scanf("%d",&s);	
+	}
+  
+    a[s]=1;
+    	printf("Class:\"Economy\"\nSeat number: %d\n",s); 
     
 }
 }
